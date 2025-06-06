3513e85a443432c409dfadceb6c1fdcbb3d8b1ef
xaimohchau@gmail.com <xaimohchau@gmail.com>
2025-06-07 02:02:14 +0300

    created base.html

--- Rcf/settings.py
+++ Rcf/settings.py
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'Rcfapp.context_processors.current_year',  # Custom context processor
             ],
         },
     },
