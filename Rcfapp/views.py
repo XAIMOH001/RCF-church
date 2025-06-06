7ddd1f832b758d884b640e79d2c7d14f26c88de3
xaimohchau@gmail.com <xaimohchau@gmail.com>
2025-06-06 20:48:15 +0300

    added new webpage files

--- Rcfapp/views.py
+++ Rcfapp/views.py
@@ -7,3 +7,5 @@ def about(request):
     return render (request, 'about.html')
 def contact(request):
     return render (request, 'contact.html')
+def sermons(request):
+    return render (request, 'sermons.html')
